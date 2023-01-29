c577e1393d995635aa6d6eb8555d11fa3ef56f4e
YuriyRysak <yuriy.risak@gmail.com>
2023-01-29 18:46:20 +0200

    friends

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,18 +1,49 @@
 import PropTypes from 'prop-types';
 import css from './FriendStyle.module.css';
 
-export const FriendList = ({avatar, name, isOline}) => (
-<li className={css.item}>
-  <span className={css.status}>{isOline}</span>
-  <img classNames={css.avatar} src={avatar} alt="User avatar" width="48" />
-  <p className={css.friendName}>{name}</p>
-</li>
+export const FriendList = ({friends}) => {
+  return(
+  <div>
+   <ul>{friends.map(({ avatar, name, isOline, id }) => (
+    <li className={css.item} key={id}>
+        <span className={css.status}>{isOline}</span>
+        <img className={css.avatar} src={avatar} alt={name} width="48" />
+        <p className={css.name}>{name}</p>
+      </li>
+   ))}
+      
+    </ul>
+</div>
 
-)
+  )};
+
+
+
+
+
+// export const FriendList = ({ friends }) => {
+//   // console.log(friends);
+
+//   return (
+//     <div className={css.friends}>
+//       <ul className={css.friends__list}>
+//         {friends.map(({ avatar, name, isOnline, id }) => (
+//           <li className={css.friends__item} key={id}>
+//             <span className={`${css.friends__status} ${css[isOnline]}`}>
+//               {isOnline}
+//             </span>
+//             <img className={css.avatar} src={avatar} alt={name} />
+//             <p className={css.friends__name}>{name}</p>
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
 
 FriendList.propTypes = {
-    avatar: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    isOnline: PropTypes.bool.isRequired,
-    id: PropTypes.number.isRequired,
-  };
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
+  id: PropTypes.number.isRequired,
+};
