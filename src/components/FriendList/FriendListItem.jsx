11730217c9dbfbd016d15dd64ab148b29929f5d7
YuriyRysak <yuriy.risak@gmail.com>
2023-01-30 14:46:38 +0200

    icon-friends

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -1,10 +1,11 @@
 import PropTypes from 'prop-types';
 import css from './FriendStyle.module.css';
+import { BsFillCircleFill } from "react-icons/bs";
 
 export const FriendListItem = ({ avatar, name, isOnline }) => (
 
   <li className={css.item}>
-    <span className={isOnline ? css.online : css.offline}></span>
+    <span> <BsFillCircleFill className={isOnline ? css.online : css.offline}/></span>
     <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
         <p className={css.name}>{name}</p>
   </li>
