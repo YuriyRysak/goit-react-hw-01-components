ee5e10019ac4202d863348a27e9c085adaaf6a81
YuriyRysak <yuriy.risak@gmail.com>
2023-01-30 14:24:21 +0200

    position

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,28 +1,27 @@
 import PropTypes from 'prop-types';
-import css from './FriendStyle.module.css';
-import { BsFillBrightnessLowFill } from "react-icons/bs";
+import { FriendListItem } from './FriendListItem';
+import css from './FriendStyle.module.css'
 
-export const FriendList = ({friends}) => {
-  return(
-  <div>
-   <ul>{friends.map(({ avatar, name, isOline, id }) => (
-    <li className={css.item} key={id}>
-    <BsFillBrightnessLowFill className={css.online-friends}/>
-        <span className={css.status}>{isOline}</span>
-        <img className={css.avatar} src={avatar} alt={name} width="48" />
-        <p className={css.name}>{name}</p>
-      </li>
-   ))}
-      
-    </ul>
-</div>
-
-  )};
-
+export const FriendList = ({ friends }) => (
+  <ul className={css.friendList}>
+    {friends.map(({id, avatar, isOnline, name}) => (
+      <FriendListItem
+        key={id}
+        avatar={avatar}
+        isOnline={isOnline}
+        name={name}
+      ></FriendListItem>
+    ))}
+  </ul>
+);
 
 FriendList.propTypes = {
-  avatar: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  isOnline: PropTypes.bool.isRequired,
-  id: PropTypes.number.isRequired,
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+    })
+  ).isRequired,
 };
--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -28,7 +28,29 @@ export const TransactionHistory = ({items}) => {
   )
 };
 
+// export const TransactionHistory = ({items}) => {
+//   return(
+//   <table className={css.transactionHistoryTable}>
+//     <thead className={css.hederRow}>
+//       <tr className={css.valuesRow}>
+//         <th className={css.columnHeader}>Type</th>
+//         <th className={css.columnHeader}>Amount</th>
+//         <th className={css.columnHeader}>Currency</th>
+//       </tr>
+//     </thead>
+//     <tbody>
+//       {items.map(({id, type, amount,currency}) => (
+//         <tr key={id} className={css.valuesRow}>
+//           <td className={css.transactionData}>{type}</td>
+//           <td className={css.transactionData}>{amount}</td>
+//           <td className={css.transactionData}>{currency}</td>
+//         </tr>
+//       ))}
+//     </tbody>
+//   </table>
+// )
 
+// };
 
 TransactionHistory.protoType = {
   items: PropTypes.arrayOf(
